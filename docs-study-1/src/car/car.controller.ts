98fca9903833159ba10bff72f79bb593a20f3bc9
YeomJaeSeon <a89541457@gmail.com>
2022-02-11 22:23:52 +0900

    nest docs - 비동기 프로바이더

--- docs-study-1/src/car/car.controller.ts
+++ docs-study-1/src/car/car.controller.ts
@@ -6,13 +6,20 @@ import { CarService } from './car.service';
 @Controller('car')
 export class CarController {
   private connection: Connection;
-  constructor(@Inject('CONNECTION') connection: Connection, private configService: ConfigService) {
+  private asyncConnection;
+  constructor(
+    @Inject('CONNECTION') connection: Connection,
+    private configService: ConfigService,
+    @Inject('ASYNC_CONNECITON') asyncConnection,
+  ) {
     this.connection = connection;
+    this.asyncConnection = asyncConnection;
   }
 
   @Get()
   getConnection() {
     console.log(this.connection.name);
     console.log(this.configService.config('hi'));
+    console.log(this.asyncConnection);
   }
 }
--- docs-study-1/src/car/car.module.ts
+++ docs-study-1/src/car/car.module.ts
@@ -20,6 +20,19 @@ const configServiceProvider = {
     process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
 };
 
+//참고로 settimeout은 Promise를 리턴하지 않는다..
+const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay));
+
+//비동기적인 동작이라 해보자...
+//팩토리 프로바이더를 통해서 비동기 프로바이더 만들수있따! - 비동기 작업 까지 어플리케이션이 시작하는걸 지연시키고 싶으면.. (ex 데이터베이스 연결 다되어야 서버 실행시키고싶으면 팩토리 프로바이더를 통해 async await으로 비동기 처리를 한다.!)
+const nonAsyncModule = {
+  provide: 'ASYNC_CONNECITON', //비동기 프로바이더이므로 DI토큰 을 이용해야하고 해당 프로바이더 주입할 땐 @Inject이용! (생성자 주입못한다 ~)
+  useFactory: async () => {
+    await wait(5000);
+    return connection;
+  },
+};
+
 @Module({
   providers: [
     {
@@ -27,6 +40,7 @@ const configServiceProvider = {
       useValue: connection,
     },
     configServiceProvider,
+    nonAsyncModule,
   ],
   controllers: [CarController],
 })
